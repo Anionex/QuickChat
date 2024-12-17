ee0ea8383b0e8ecdeea422e53db54f4e3ca561b0
anion <1005128408@qq.com>
2024-12-17 15:32:04 +0800

    v1

--- app.py
+++ app.py
@@ -6,21 +6,22 @@ else:
 import gradio as gr
 import dotenv
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(override=True)
 
-client = OpenAI(            
-                api_key=os.getenv("OPENAI_API_KEY"),
-            base_url=os.getenv("OPENAI_API_BASE"),  
-                )
-
+selected_model = "llama-3.2-90b-text-preview"
+input_openai_api_base = os.getenv("OPENAI_API_BASE")
 def predict(message, history, system_prompt):
+    client = OpenAI(            
+                api_key=os.getenv("OPENAI_API_KEY"),
+            base_url=input_openai_api_base,  
+                )
     history_openai_format = [{"role": "system", "content": system_prompt}]
     for human, assistant in history:
         history_openai_format.append({"role": "user", "content": human })
         history_openai_format.append({"role": "assistant", "content":assistant})
     history_openai_format.append({"role": "user", "content": message})
   
-    response = client.chat.completions.create(model='llama-3.2-90b-text-preview',
+    response = client.chat.completions.create(model=selected_model,
     messages= history_openai_format,
     temperature=1.0,
     stream=True)
@@ -37,10 +38,23 @@ CSS ="""
 #chatbot { flex-grow: 1; overflow: auto;}
 """
 with gr.Blocks(css=CSS) as demo:
+    # with gr.Accordion("Advanced options"):
+    #     selected_model = gr.Radio(label="model", choices=["llama-3.2-90b-text-preview", "llama-3.1-sonar-large-128k-online"], value="llama-3.2-90b-text-preview", interactive=True)
+    #     input_openai_api_base = gr.Textbox(label="openai api base", placeholder=input_openai_api_base)
+    
     system_prompt = gr.Textbox(label="system", placeholder="system prompt here...")
+    gr.Examples(
+        examples=[
+            "翻译用户提供的文案为中文，翻译时考虑上下文。删除括号内参考文献文字内容。",
+            "作为一名人工智能行业内专家，找出用户提供的文案中不正确的信息，并进行修正。",
+        ],
+        inputs=[system_prompt],
+    )
     chatbot = gr.ChatInterface(
         predict,
         additional_inputs=[system_prompt],
     )
 
 demo.launch(server_port=18080)
+
+
